6e2afe78ee8e21b1d7b3aa336e7cdf6d943106be
Azael Reyes <azael.devel@gmail.com>
2020-12-15 01:17:40 -0800

    Packing for linux

--- src/clientdb.cc
+++ src/clientdb.cc
@@ -1,3 +1,24 @@
+/**
+ *
+ *  This file is part of octetos-db-abstract.
+ *  octetos-db-abstract is a C++ API to access database, abstract component.
+ *  Copyright (C) 2018  Azael Reyes
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,10 +34,11 @@
     #include <octetos/core/Error.hh>
 #endif
 
-
 #ifdef HAVE_CONFIG_H
     #include "config.h"
 #elif defined WINDOWS_MINGW && defined CODEBLOCKS
+    #include "config-cb.h"
+#elif defined LINUX && defined CODEBLOCKS
     #include "config-cb.h"
 #endif
 #include "clientdb.hh"
--- src/clientdb.hh
+++ src/clientdb.hh
@@ -1,8 +1,30 @@
 #ifndef OCTETOS_CLIENTDB_HH
 #define OCTETOS_CLIENTDB_HH
 
+/**
+ *
+ *  This file is part of octetos-db-abstract.
+ *  octetos-db-abstract is a C++ API to access database, abstract component.
+ *  Copyright (C) 2018  Azael Reyes
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * */
 
 #if defined WINDOWS_MINGW
+    #include <Artifact.hh>
+#elif defined LINUX && defined CODEBLOKS
     #include <Artifact.hh>
 #else
     #include <octetos/core/Artifact.hh>
